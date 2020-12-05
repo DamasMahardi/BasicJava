9176474e0e287146257c49d646f718294b499f68
DamasMahardi <damasmahardi234@gmail.com>
2020-12-05 12:45:14 +0700

    add class ifelse

---
+++ src/com/company/Ifelse.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Ifelse {
+    public static void main(String[] args) {
+        int i = 50;
+        if (i > 10){
+            System.out.println("Nilainya lebih dari 10");
+        }else {
+            System.out.println("nilainya kurang dari 10");
+        }
+
+    }
+
+
+
+
+}
